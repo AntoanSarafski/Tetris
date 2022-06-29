6d37f5e9ca5ca8acd84cdabfb7db55710d6e65b0
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-06-29 12:09:46 +0300

    <The method for drawing boarders is ready.>

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -4,9 +4,65 @@ namespace Tetris
 {
     class Program
     {
+        static int TetrisRows = 20;
+        static int TetrisCols = 10;
+        static int InfoCols = 10;
+        static int ConsoleRows = 1 + TetrisRows + 1;
+        static int ConsoleCols = 1 + TetrisCols + 1 + InfoCols + 1;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting Tetris.");
+            Console.Title = "Tetris v1.0";
+            Console.WindowHeight = ConsoleRows + 1;
+            Console.WindowWidth = ConsoleCols;
+            Console.BufferHeight = ConsoleRows + 1;
+            Console.BufferWidth = ConsoleCols;
+            DrawBoard();
+            Console.ReadLine();
+        }
+
+
+        static void DrawBoard()
+        {
+            string line = "╔";
+            line += new string('═', TetrisCols);
+            /* for (int i = 0; i < TetrisCols; i++)
+            {
+                line += "═";
+            } */
+            line += "╦";
+            line += new string('═', InfoCols);
+            line += "╗";
+            Console.WriteLine(line);
+
+            for (int i = 0; i < TetrisRows; i++)
+            {
+                string middleLine = "║";
+                middleLine += new string(' ', TetrisCols);
+                middleLine += "║";
+                middleLine += new string(' ', InfoCols);
+                middleLine += "║";
+                Console.WriteLine(middleLine);
+            }
+
+            string endLine = "╚";
+            endLine += new string('═', TetrisCols);
+            endLine += "╩";
+            endLine += new string('═', InfoCols);
+            endLine += "╝";
+            Console.WriteLine(endLine);
+        }
+
+        static void Write(
+            string text,
+            int row,
+            int col,
+            ConsoleColor color = ConsoleColor.Yellow)
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(col, row);
+            Console.Write(text);
+            Console.ResetColor();
         }
     }
 }
