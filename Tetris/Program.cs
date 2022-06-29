77c82cf6ec46bb8645249ec32f0736bf13095d82
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-06-29 15:54:22 +0300

    <Implementation of moving figures.>

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Tetris
@@ -6,29 +7,73 @@ namespace Tetris
     class Program
     {
         // Settings
-        static int TetrisRows = 30;
-        static int TetrisCols = 15;
-        static int InfoCols = 15;
+        static int TetrisRows = 20;
+        static int TetrisCols = 10;
+        static int InfoCols = 10;
         static int ConsoleRows = 1 + TetrisRows + 1;
         static int ConsoleCols = 1 + TetrisCols + 1 + InfoCols + 1;
+        static List<bool[,]> TetrisFigure = new List<bool[,]>()
+        {
+            new bool [,] // ----
+            {
+                { true, true, true, true }
+            },
+            new bool [,] // O
+            {
+                {true, true, },
+                {true, true, }
+            }, 
+            new bool [,] // T
+            {
+                {false, true, false },
+                {true, true, true }
+            }, 
+
+            new bool [,] // S
+            {
+                {false, true, true },
+                {true, true, false }
+            },
+             new bool [,] // Z
+            {
+                {true, true, false },
+                {false, true, true }
+            },
+            new bool [,] // J
+            {
+                {true, false, false },
+                {true, true, true }
+            }, 
+            new bool [,] // L
+            {
+                {false, false, true, },
+                {true, true, true, }
+            }, 
+        };
 
         // State
-        static int score = 0;
-        private static object thread;
+        static int Score = 0;
+        static int Frame = 0;
+        static int FramesToMoVeFigure = 15;
+        static int CurrentFigureIndex = 2;
+        static int CurrentFigureRow = 0;
+        static int CurrentFigureCol = 0;
+        static bool[,] TetrisField = new bool[TetrisRows, TetrisCols];
 
         static void Main(string[] args)
         {
             Console.Title = "Tetris v1.0";
             Console.CursorVisible = false;
-            Console.WindowHeight = ConsoleRows + 1;
-            Console.WindowWidth = ConsoleCols;
-            Console.BufferHeight = ConsoleRows + 1;
-            Console.BufferWidth = ConsoleCols;
-            DrawBoard();
-            DrawInfo();
+            // Console.WindowHeight = ConsoleRows + 1;
+            // Console.WindowWidth = ConsoleCols;
+            Console.SetWindowSize(ConsoleCols, ConsoleRows + 1);
+            // Console.BufferHeight = ConsoleRows + 1;
+            // Console.BufferWidth = ConsoleCols;
+            Console.SetBufferSize(ConsoleCols, ConsoleRows + 1);
+            
             while (true)
             {
-                score++;
+                Frame++;
                 // user input
                 if (Console.KeyAvailable)
                 {
@@ -37,17 +82,56 @@ namespace Tetris
                     {
                         Environment.Exit(0);
                     }
-                    
+                    if (key.Key == ConsoleKey.LeftArrow || key.Key == ConsoleKey.A)
+                    {
+                        // TODO: Move current figure left
+                        CurrentFigureCol--; // TODO: Out of range !!!
+                    }
+                    if (key.Key == ConsoleKey.RightArrow || key.Key == ConsoleKey.D)
+                    {
+                        // TODO: Move current figure right
+                        CurrentFigureCol++; // TODO: Out of range !!!
+                        
+                    }
+                    if (key.Key == ConsoleKey.DownArrow || key.Key == ConsoleKey.S)
+                    {
+                        Frame = 0;
+                        CurrentFigureRow++;
+                        // TODO: Move current figure down
+                    }
+                    if (key.Key == ConsoleKey.Spacebar 
+                        || key.Key == ConsoleKey.W
+                        || key.Key == ConsoleKey.UpArrow)
+                    {
+                        // TODO: Implement 90-degree rotation of the current figure.
+                    }
                 }
 
-                // change state
+                // Update the game state
+                if (Frame % FramesToMoVeFigure == 0)
+                {
+                    CurrentFigureRow++;
+                    Frame = 0;
+                    Score++;
+                }
+                // TODO: if (Collision())
+                //{
+                     // AddCurrentFigure()
+                     // CheckForFullLines()
+                     // if(lines remove) 
+                //}
 
                 // redraw UI
                 DrawBoard();
                 DrawInfo();
+                // TODO: DrawTetrisField()
+                DrawCurrentFigure();
+
+                
                 Thread.Sleep(40);
             }
         }
+
 
 
         static void DrawBoard()
@@ -85,7 +169,24 @@ namespace Tetris
         static void DrawInfo()
         {
             Write("Score:", 1, 3 + TetrisCols);
-            Write(score.ToString(), 2, 3 + TetrisCols);
+            Write(Score.ToString(), 2, 3 + TetrisCols);
+            Write("Frame:", 4, 3 + TetrisCols);
+            Write(Frame.ToString(), 5, 3 + TetrisCols);
+        }
+
+        static void DrawCurrentFigure()
+        {
+            var currentFigure = TetrisFigure[CurrentFigureIndex];
+            for (int row = 0; row < currentFigure.GetLength(0); row++)
+            {
+                for (int col = 0; col < currentFigure.GetLength(1); col++)
+                {
+                    if (currentFigure[row, col])
+                    {
+                        Write("*", row + 1 + CurrentFigureRow, col + 1 + CurrentFigureCol);
+                    }
+                }
+            }
         }
 
         static void Write(string text,int row,int col,ConsoleColor color = ConsoleColor.Yellow)
